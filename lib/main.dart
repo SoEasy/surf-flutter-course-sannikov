a5a162a552de365038dfa2b04c799f7a9d595244
vhaldemario <sowa@outlook.com>
2020-12-03 22:36:57 +0200

    task_4_1: scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(App());
@@ -9,88 +10,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'First app',
-      home: new Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MyFirstWidget(),
-            MyFirstStatefulWidget()
-          ],
-        ),
-      )
+      home: SightListScreen()
     );
   }
 }
 
-class MySmallText extends StatelessWidget {
-  final String child;
-
-  MySmallText(this.child);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.only(bottom: 10),
-        child: Text(
-          this.child,
-          style: TextStyle(fontSize: 14)
-        ),
-      )
-    );
-  }
-}
-
-
-class MyFirstWidget extends StatelessWidget {
-  int count = 0;
-
-  // Контекста нету в полях класса StatelessWidget, ошибка компиляции
-  // getContextRuntimeType() {
-  //   return context.runtimeType;
-  // }
-
-  @override
-  Widget build(BuildContext context) {
-    count += 1;
-    print('Stateless counter is $count and it always stay 1');
-    return Container(
-      child: Center(
-        child: MySmallText(
-          'Hello! Stateless widget $count'
-        )
-      )
-    );
-  }
-}
-
-class MyFirstStatefulWidget extends StatefulWidget {
-  const MyFirstStatefulWidget();
-
-  @override
-  _MyFirstStatefulWidgetState createState() => _MyFirstStatefulWidgetState();
-}
-
-class _MyFirstStatefulWidgetState extends State<MyFirstStatefulWidget> {
-  int count = 0;
-
-  getContextRuntimeType() {
-    return context.runtimeType;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    print(getContextRuntimeType());
-
-    count += 1;
-    print('Statefull counter is $count and it always grow up!');
-    return Container(
-      child: Center(
-        child: MySmallText(
-          'Hello! Statefull widget $count'
-        )
-      )
-    );
-  }
-}
---
+++ lib/ui/screen/sight_list_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SightListScreen extends StatefulWidget {
+  @override
+  _SightListWidgetState createState() => _SightListWidgetState();
+}
+
+class _SightListWidgetState extends State<SightListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+      appBar: AppBar(
+        title: Text('Список интересных мест')
+      ),
+    );
+  }
+}
