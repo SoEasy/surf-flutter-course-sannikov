845a92acead27b6b97fbd011b4e3fee9b215ee22
vhaldemario <sowa@outlook.com>
2020-12-06 21:10:09 +0200

    task_4_2_1: RichText & styles inheritance

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -16,17 +16,30 @@ class _SightListWidgetState extends State<SightListScreen> {
         // Не знаю надо ли было делать отступы как в макете, сделал
         title: Padding(
           padding: EdgeInsets.only(top: 40, bottom: 16),
-          child: Text(
-            PlacesTexts.SightListTitleBreak,
-            textAlign: TextAlign.start,
-            maxLines: 2,
-            style: TextStyle(
-              color: PlacesColors.White_Main,
-              fontSize: 32,
-              height: 1.125, // line-height 36
-              fontFamily: 'Roboto',
-              fontWeight: FontWeight.w700,
-            ),
+          child: RichText(
+            text: TextSpan(
+              style: TextStyle(
+                color: PlacesColors.White_Main,
+                fontSize: 32,
+                height: 1.125, // line-height 36
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w700,
+              ),
+              children: [
+                TextSpan(
+                  text: 'Список\n',
+                  style: TextStyle(
+                    color: Colors.green
+                  )
+                ),
+                TextSpan(
+                  text: 'интересных мест',
+                  style: TextStyle(
+                    color: Colors.yellow
+                  )
+                )
+              ]
+            )
           ),
         ),
         centerTitle: false,
