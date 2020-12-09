7366524eab7825f289763034263f8bea0600dfa9
vhaldemario <sowa@outlook.com>
2020-12-09 22:10:25 +0200

    task_4_3_3: code format

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -3,31 +3,34 @@ import 'domain/sight.dart';
 /// Моковые данные
 final List<Sight> mocks = [
   new Sight(
-      name: 'Горхол',
-      lat: 59.446667,
-      lon: 24.752778,
-      url: 'https://i.ytimg.com/vi/3W4hWG12Pdg/maxresdefault.jpg',
-      details:
-          'Заброшка в Таллине, созданная под оперный театр. Театр не удался, зато там снимали Довод',
-      type: 'Строение',
-      workTime: 'С 9 до 18'),
+    name: 'Горхол',
+    lat: 59.446667,
+    lon: 24.752778,
+    url: 'https://i.ytimg.com/vi/3W4hWG12Pdg/maxresdefault.jpg',
+    details:
+        'Заброшка в Таллине, созданная под оперный театр. Театр не удался, зато там снимали Довод',
+    type: 'Строение',
+    workTime: 'С 9 до 18',
+  ),
   new Sight(
-      name: 'Аэродром Смирных',
-      lat: 49.738889,
-      lon: 142.861111,
-      url:
-          'https://avatars.mds.yandex.net/get-zen_doc/1586459/pub_5dec9bdd3d873600ae285314_5dec9c03e6cb9b00aee527e9/scale_1200',
-      details: 'Аэропорт на острове',
-      type: 'Строение',
-      workTime: 'Круглостуочно'),
+    name: 'Аэродром Смирных',
+    lat: 49.738889,
+    lon: 142.861111,
+    url:
+        'https://avatars.mds.yandex.net/get-zen_doc/1586459/pub_5dec9bdd3d873600ae285314_5dec9c03e6cb9b00aee527e9/scale_1200',
+    details: 'Аэропорт на острове',
+    type: 'Строение',
+    workTime: 'Круглостуочно',
+  ),
   new Sight(
-      name: 'Сен-Жерменский дворец и его предместья',
-      lat: 48.881389,
-      lon: 2.096389,
-      url:
-          'https://upload.wikimedia.org/wikipedia/commons/3/31/Ch%C3%A2teau_de_Saint-Germain-en-Laye01.jpg',
-      details:
-          'Знаменитый дворец во Франции, в окрестностях которого писал великий Виктор Гюго',
-      type: 'Территория',
-      workTime: 'Круглостуочно')
+    name: 'Сен-Жерменский дворец и его предместья',
+    lat: 48.881389,
+    lon: 2.096389,
+    url:
+        'https://upload.wikimedia.org/wikipedia/commons/3/31/Ch%C3%A2teau_de_Saint-Germain-en-Laye01.jpg',
+    details:
+        'Знаменитый дворец во Франции, в окрестностях которого писал великий Виктор Гюго',
+    type: 'Территория',
+    workTime: 'Круглостуочно',
+  )
 ];
