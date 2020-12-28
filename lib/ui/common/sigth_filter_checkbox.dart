4270c3e12ee3a46daf27c514aa9b03a3af2e4463
vhaldemario <sowa@outlook.com>
2020-12-28 20:36:22 +0200

    5_2_2: enum sight type

--- lib/ui/common/sigth_filter_checkbox.dart
+++ lib/ui/common/sigth_filter_checkbox.dart
@@ -3,23 +3,36 @@ import 'package:places/shared/places_colors.dart';
 import 'package:places/shared/places_fonts.dart';
 import 'package:places/ui/common/icons.dart';
 
+/// Типы интересных мест
+enum SightType {
+  hotel,
+  restaurant,
+  special,
+  park,
+  museum,
+  cafe
+}
+
 /// Чекбокс-киконка для экрана фильтров
 class SightFilterCheckbox extends StatefulWidget {
   final SightIconBase icon;
   final bool value;
+
   /// имя поля, подобное name в html-контролах.
   /// Именно оно будет выстреливать в коллбэк onChanged,
   /// чтобы наверху работать с Map
-  final String name;
+  final SightType sightType;
+
   /// Коллбэк на изменение, вызывается с name
-  final ValueChanged<String> onChanged;
-  ///
+  final ValueChanged<SightType> onChanged;
+
+  /// текст под иконкой
   final String title;
 
   SightFilterCheckbox({
     @required this.icon,
     @required this.value,
-    @required this.name,
+    @required this.sightType,
     this.onChanged,
     this.title,
   });
@@ -47,16 +60,14 @@ class _SightFilterCheckboxState extends State<SightFilterCheckbox> {
                 child: InkWell(
                   borderRadius: BorderRadius.circular(64),
                   onTap: () {
-                    if (widget.onChanged != null) {
-                      widget.onChanged(widget.name);
-                    }
+                    widget.onChanged?.call(widget.sightType);
                   },
                   child: Center(
                     child: widget.icon,
                   ),
                 ),
               ),
-              if (widget.value == true)
+              if (widget.value)
                 Positioned(
                   child: SightIconChoice(),
                   right: -4,
